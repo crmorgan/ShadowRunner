4f6a25eb4cc1a4ff10ff9fe60c9959e13026f0bf
Chris <chris-morgan@live.com>
2015-04-14 14:57:03 -0600

    Small improvements to the usage text

--- RunnerConsole/ConsoleWriter.cs
+++ RunnerConsole/ConsoleWriter.cs
@@ -7,9 +7,13 @@ namespace ShadowRunner.RunnerConsole
 	{
 		public static void WriteBanner()
 		{
-			var version = Assembly.GetEntryAssembly().GetName().Version;
-
-			Console.WriteLine( "Shadow Runner v{0}", version );
+			Console.WriteLine(@"   _____ __              __                 ____                             ");
+			Console.WriteLine(@"  / ___// /_  ____ _____/ /___ _      __   / __ \__  ______  ____  ___  _____");
+			Console.WriteLine(@"  \__ \/ __ \/ __ `/ __  / __ \ | /| / /  / /_/ / / / / __ \/ __ \/ _ \/ ___/");
+			Console.WriteLine(@" ___/ / / / / /_/ / /_/ / /_/ / |/ |/ /  / _, _/ /_/ / / / / / / /  __/ /    ");
+			Console.WriteLine(@"/____/_/ /_/\__,_/\__,_/\____/|__/|__/  /_/ |_|\__,_/_/ /_/_/ /_/\___/_/     ");
+			Console.WriteLine();
+			Console.WriteLine("Version v{0}", Assembly.GetEntryAssembly().GetName().Version);
 			Console.WriteLine();
 		}
 
@@ -19,17 +23,21 @@ namespace ShadowRunner.RunnerConsole
 			Console.WriteLine();
 			Console.WriteLine("SHADOWRUNNER application [arguments]");
 			Console.WriteLine();
-			Console.WriteLine("  application    Specifies the full path and filename of the console application to run.");
-			Console.WriteLine("  arguments      The arguments that are passed to the target console application.");
+			Console.WriteLine(@"  application    Specifies the full path and filename of the console application to run.");
+			Console.WriteLine(@"  arguments      The arguments that are passed to the target console application.");
+			Console.WriteLine();
+			Console.WriteLine(@"Example: shadowrunner C:\MyConsole\MyConsole.exe someargument" );
+			Console.WriteLine();
+
 		}
 
-		public static void WriteRunStarting(AppDomain appDomain)
+		public static void WriteRunStarting( AppDomain appDomain )
 		{
 			Console.WriteLine("Running the {0} application", appDomain.FriendlyName);
 			Console.WriteLine();
 		}
 
-		public static void WriteRunComplete( )
+		public static void WriteRunComplete()
 		{
 			Console.WriteLine("Run completed");
 			Console.WriteLine();
